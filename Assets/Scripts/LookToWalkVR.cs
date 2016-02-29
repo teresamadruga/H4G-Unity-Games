9632963363bfdeab994ed5cc740c9a25352024b3
Teresa Madruga <madrugat@gmail.com>
2016-02-29 20:50:15 +0100

    movimiento en direccion mirada implementado correctamente

---
+++ Assets/Scripts/LookToWalkVR.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookToWalkVR : MonoBehaviour 
+{
+	private CharacterController charControl;
+	private Clicker click;
+	private bool walking=false;
+	public float speed = 0.6F;
+	void Start ()
+	{
+		charControl=GetComponent<CharacterController>();
+	}
+	void Update ()
+	{
+		if (click.clicked())
+		{
+			walking = !walking;
+		}
+		if (walking)
+		{
+			charControl.SimpleMove (Camera.main.transform.forward*speed);
+		}
+	}
+}
